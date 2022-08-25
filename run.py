1cfdaced5c3a8732fb779997752b75aeab2e7add
Sean Clark <seaniboy2009@yahoo.co.uk>
2022-08-25 13:59:45 +0000

    collect last 5 sales data in list of columns

--- run.py
+++ run.py
@@ -90,6 +90,20 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    Get last 5 sales for each sandwich and return the average + 10%
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
+
 def main():
     """
     Run all program functions
@@ -102,4 +116,5 @@ def main():
 
 
 print("Welcome to love sandwiches data automation")
-main()
+# main()
+get_last_5_entries_sales()
